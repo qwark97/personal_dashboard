ed8eef3984fb1cd5307a95390a9a4010764ed815
qwark97 <37438072+qwark97@users.noreply.github.com>
2019-06-24 15:20:47 +0200

    added simple validation functions

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 import geocoder
 from darksky import forecast
-def get_temp():
+import re
+def get_weather():
     API_KEY = '71f646055b315e115ef69e0b11d1eee4'
     lat, lng = geocoder.ip('me').latlng
     place = forecast(API_KEY, lat, lng)
@@ -8,3 +9,20 @@ def get_temp():
     temp = f_to_c(place.currently.temperature)
     summary = place.currently.summary
     return (summary, str(temp)+chr(176)+'C')
+
+def validate_name(user_name):
+    if not user_name:
+        return False
+    if len(user_name) < 3 or len(user_name) > 20:
+        return False
+    return True
+
+def validate_pass(password):
+    if password:
+        return True
+    return False
+
+def validate_email(email):
+    if re.match(r'^.+@\w+\.\w{2,3}$', email):
+        return True
+    return False
